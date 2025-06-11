e4709e658f722e1c49fb1f43c69d03cc450592f6
ЮсинаАВ <alina.yusina@yandex.ru>
2025-06-11 11:14:57 +0300

    нифига не получилось

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,99 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const browserSync = require('browser-sync').create();
-const cleanCSS = require('gulp-clean-css');
-const fileInclude = require('gulp-file-include');
-const {src, dest, watch, series, parallel} = gulp;
+import gulp from 'gulp';
+import fileInclude from 'gulp-file-include';
+import { deleteAsync } from 'del';
+import  * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import cleanCSS from 'gulp-clean-css';
+import autoprefixer from 'gulp-autoprefixer';
+import htmlmin from 'gulp-htmlmin';
+import browserSync from 'browser-sync';
+import webp from 'gulp-webp';
+import responsive from 'gulp-sharp-responsive';
+import newer from "gulp-newer";
+
+const sass = gulpSass(dartSass);
+const bs = browserSync.create();
 
 const paths = {
     html: {
-        src: 'src/html/*.html',
+        src: 'src/pages/**/*.html',
         dest: 'dist/',
-        watch: 'src/html/**/*.html'
+        watch: ['src/pages/**/*.html', 'src/components/**/*.html'],
     },
-    scss: {
-        src: 'src/scss/style.scss',
-        dest: 'dist/css/',
-        watch: 'src/scss/**/*.scss'
+    styles: {
+        src: 'src/scss/main.scss',
+        dest: 'dist/assets/css/',
+        watch: 'src/scss/**/*.scss',
     },
-}
+    assets: {
+        src: 'src/assets/**/*',
+        dest: 'dist/assets/',
+    },
+    images: {
+        src: 'src/images/**/*',
+        dest: 'dist/assets/images',
+    }
+};
 
+export const html = () =>
+    gulp
+        .src(paths.html.src)
+        .pipe(
+            fileInclude({
+                prefix: '@@',
 
+                basepath: 'src/components/',
+            })
+        )
+        .pipe(
+            htmlmin({
+                collapseWhitespace: false,
+                removeComments: true,
+            })
+        )
+        .pipe(gulp.dest(paths.html.dest))
+        .pipe(bs.stream());
 
-function html() {
-    return src(paths.html.src)
-        .pipe(fileInclude({
-            prefix: '@@',
-            basepath: '@file'
-        }))
-        .pipe(dest(paths.html.dest))
-        .pipe(browserSync.stream());
-}
-
-function styles() {
-    return src(paths.scss.src)
+export const styles = () =>
+    gulp
+        .src(paths.styles.src)
         .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS({level: 2}))
-        .pipe(dest(paths.scss.dest))
-        .pipe(browserSync.stream());
-}
+        .pipe(autoprefixer({ cascade: false }))
+        .pipe(cleanCSS({ level: 2 }))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(bs.stream());
 
-function serve() {
-    browserSync.init({
+const imagesToWebp = () =>
+    gulp.src(paths.images.src, { encoding: false })
+        .pipe(newer(paths.images.dest))
+        .pipe(webp({ quality: 80 }))
+        .pipe(gulp.dest(paths.images.dest))
+
+
+// Копирование ассетов
+export const assets = () =>
+    gulp.src(paths.assets.src).pipe(gulp.dest(paths.assets.dest));
+
+// Очистка
+export const clean = () => deleteAsync(['dist']);
+
+// Сервер
+export const serve = () => {
+    bs.init({
         server: {
-            baseDir: 'dist/'
+            baseDir: './dist',
         },
-        notify: false
+        notify: false,
+        open: true,
+        cors: true,
     });
 
-    watch(paths.scss.watch, styles);
-    watch(paths.html.watch, html);
-}
+    gulp.watch(paths.styles.watch, styles);
+    gulp.watch(paths.html.watch, html);
+    gulp.watch(paths.assets.src, assets);
+    gulp.watch(paths.images.src, imagesToWebp);
+};
 
-
-exports.default = series(
-    parallel(html, styles),
-    serve
-);
+// Сборка
+export const build = gulp.series(clean, gulp.parallel(styles, html, assets, imagesToWebp));
+export default gulp.series(build, serve);
